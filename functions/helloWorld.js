f3e92ad43387c87f46eb046a2ae6bb4323fee1db
Piyush singh <114461687+thisthat587@users.noreply.github.com>
2024-03-15 08:05:22 +0530

    first

---
+++ functions/helloWorld.js
@@ -0,0 +1,37 @@
+// functions/helloWorld.js
+
+exports.handler = async (event, context) => {
+  try {
+    // Process the incoming request
+    const length = event.queryStringParameters && event.queryStringParameters.length ? event.queryStringParameters.length : 8;
+    const characters = 'abcdefghijklmnopqrstuvwxyz1234567890~!@#$%^&*()_+{}:><?/.,;][|';
+    let password = '';
+
+    for (let i = 0; i < length; i++) {
+      const index = Math.floor((Math.random() * (characters.length - 1)) + 1)
+      password += characters.charAt(index);
+    }
+
+    // Construct the response body
+    const responseBody = {
+      message: password,
+      event: event,
+      context: context
+    };
+
+    // Return the response
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(responseBody)
+    };
+  } catch (error) {
+    // Handle any errors
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Internal Server Error' })
+    };
+  }
+};
---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import './App.css'
+function App() {
+  const [message, setMessage] = useState('');
+  const [length, setName] = useState(8);
+
+  const handleClick = async () => {
+    try {
+      // Make a GET request to the Netlify function endpoint
+      const response = await fetch(`/api/helloWorld?length=${length}`);
+
+      // Parse the JSON response
+      const data = await response.json();
+      // Update the message state with the response message
+      setMessage(data.message);
+      // setName('')
+    } catch (error) {
+      // Handle any errors
+      console.error('Error:', error);
+      setMessage('An error occurred. Please try again later.');
+    }
+  };
+
+  return (
+    <div className="App">
+      <div className='len-container'>
+        <p>Length</p>
+        <p>:</p>
+
+        <input
+
+          type="text"
+          value={length}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <button onClick={handleClick}>Generate Password</button>
+      <div className='pass-container'>
+        <p>PASSWORD </p>
+        <p>:</p>
+        <input
+          type="text"
+          value={message}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
