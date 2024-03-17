602aabde100124e3396dc00b7ceac0e368ffc4c5
Piyush singh <114461687+thisthat587@users.noreply.github.com>
2024-03-17 22:12:30 +0530

    modified

--- functions/passwordApi.js
+++ functions/passwordApi.js
@@ -2,14 +2,40 @@ export const handler = async (event, context) => {
   try {
     // Process the incoming request
     const length = event.queryStringParameters && event.queryStringParameters.length ? event.queryStringParameters.length : 8;
-    const characters = 'abcdefghijklmnopqrstuvwxyz1234567890~!@#$%^&*()_+{}:><?/.,;][|';
+    const lowercase = event.queryStringParameters.lowercase
+    const uppercase = event.queryStringParameters.uppercase
+    const numbers = event.queryStringParameters.numbers
+    const symbols = event.queryStringParameters.symbols
+    console.log(lowercase)
+    console.log(uppercase)
+    console.log(numbers)
+    console.log(symbols)
+    // const characters = 'abcdefghijklmnopqrstuvwxyz1234567890~!@#$%^&*()_+{}:><?/.,;][|';
+    let characters = '';
+    const lowerCaseCharacters = 'abcdefghijklmnopqrstuvwxyz'
+    const upperCaseCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    const numbersCharacters = '1234567890'
+    const specialCharacters = '~!@#$%^&*()_+{}:><?/.,;][|'
     let password = '';
+
+    if (lowercase === 'true') {
+      characters += lowerCaseCharacters;
+    }
+    if (uppercase === 'true') {
+      characters += upperCaseCharacters;
+    }
+    if (numbers === 'true') {
+      characters += numbersCharacters;
+    }
+    if (symbols === 'true') {
+      characters += specialCharacters
+    }
 
     for (let i = 0; i < length; i++) {
       const index = Math.floor((Math.random() * (characters.length - 1)) + 1)
       password += characters.charAt(index);
     }
-
+    console.log(password)
     // Construct the response body
     const responseBody = {
       message: password,
--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,165 @@
 import React, { useState } from 'react';
 import './App.css'
+
+
 function App() {
-    const [message, setMessage] = useState([]);
-    const [length, setName] = useState(8);
+    const [Password, setPassword] = useState('');
+    const [length, setLength] = useState(8);
+    const [lowerCase, setLowerCase] = useState(true);
+    const [upperCase, setUpperCase] = useState(false);
+    const [numbers, setNumbers] = useState(false);
+    const [symbols, setSymbols] = useState(false);
 
-    const handleClick = async () => {
+    const getPassword = async () => {
         try {
-            // Make a GET request to the Netlify function endpoint
-            const response = await fetch(`/api/passwordApi?length=${length}`);
-            console.log(response)
-            // Parse the JSON response
+            const response = await fetch(`/api/passwordApi?length=${length}&lowercase=${lowerCase}&uppercase=${upperCase}&numbers=${numbers}&symbols=${symbols}`);
             const data = await response.json();
-            // Update the message state with the response message
-            setMessage(data.message);
-            // setName('')
-
+            setPassword(data.message);
+            
         } catch (error) {
             // Handle any errors
             console.error('Error:', error);
-            setMessage('An error occurred. Please try again later.');
+            setPassword('An error occurred. Please try again later.');
         }
     };
 
     return (
-        <div className="App">
-            <div className='len-container'>
-                <p>Length</p>
-                <p>:</p>
-                <input
-                    type="text"
-                    value={length}
-                    onChange={(e) => setName(e.target.value)}
-                />
+        <div >
+            <div >
+                <div className='p-4 flex justify-left rounded-full shadow-lg  bg-cyan-500 shadow-red-400 border items-center'>
+                    <label
+                        className='font-bold text-xl'
+                        htmlFor="lower-case"
+                    >Lowercase Characters</label>
+                    <input
+                        type='checkbox'
+                        className='h-6 w-6 ml-20'
+                        id='lower-case'
+                        checked={lowerCase}
+                        onChange={(e) => {
+                            setLowerCase(e.target.checked)
+                        }}
+                    />
+                </div>
+                <div className='p-4 flex justify-left rounded-full shadow-lg bg-cyan-500 shadow-red-400 border items-center'>
+                    <label
+                        className='font-bold text-xl'
+                        htmlFor="upper-case"
+                    >Uppercase Characters</label>
+                    <input
+                        type="checkbox"
+                        className=' h-6 w-6 ml-20 '
+                        id='upper-case'
+                        checked={upperCase}
+                        onChange={(e) => {
+                            setUpperCase(e.target.checked)
+                        }}
+                    />
+                </div>
+                <div className='p-4 flex justify-left rounded-full shadow-lg bg-cyan-500 shadow-red-400 border items-center'>
+                    <label
+                        className='font-bold text-xl mr-1'
+                        htmlFor="numbers"
+                    >Numbers</label>
+                    <input
+                        type="checkbox"
+                        className='h-6 w-6 ml-48'
+                        id='numbers'
+                        checked={numbers}
+                        onChange={(e) => {
+                            setNumbers(e.target.checked)
+                        }}
+                    />
+                </div>
+                <div className='p-4 flex justify-left rounded-full shadow-lg bg-cyan-500 shadow-red-400 border items-center'>
+                    <label
+                        className='font-bold text-xl mr-3'
+                        htmlFor="symbols"
+                    >Symbols</label>
+                    <input
+                        type="checkbox"
+                        className='h-6 w-6 mr-6 ml-48 '
+                        id='symbols'
+                        checked={symbols}
+                        onChange={(e) => {
+                            setSymbols(e.target.checked)
+                        }}
+                    />
+                </div>
+                <div className='p-4 flex rounded-full shadow-lg shadow-red-400 bg-cyan-500 border  justify-center items-center'>
+                    <label
+                        className='font-bold text-xl'
+                        htmlFor="length"
+                    >Length</label>
+                    <input
+                        type="range"
+                        className='h-6 w-48 ml-20'
+                        max={80}
+                        min={8}
+                        id='length'
+                        value={length}
+                        onChange={(e) => {
+                            setLength(e.target.value)
+                        }}
+                    />
+                    <p className="ml-8 bg-white p-2 font-extrabold text-black rounded-full shadow-black shadow-lg">{length}</p>
+
+                </div>
             </div>
-            <button onClick={handleClick}>Generate Password</button>
-            <div className='pass-container'>
-                <p>PASSWORD </p>
-                <p>:</p>
+            <div className='mt-8'>
+                <button
+                    className=' shadow-md shadow-orange-300 bg-cyan-300 rounded-full text-black font-bold'
+                    onClick={getPassword}
+                >Generate Password</button>
+            </div>
+            <div
+                className='mt-4'
+            >
                 <input
                     type="text"
-                    value={message}
-                    readOnly
+                    className='bg-pink-200 text-black font-semibold rounded-xl text-xl p-4'
+                    value={Password}
+                    onChange={(e) => [
+                        setPassword(e.target.value)
+                    ]}
+                    placeholder='PASSWORD'
                 />
-                {/* {message.map((each, index) => (
-                    <p key={index}>name {index}: {each.name}</p>
-                ))} */}
-                {/* <p>{message}</p> */}
             </div>
         </div>
-    );
+    )
+
+
+
+
+
+    // return (
+    //     <div>
+    //         <div className='len-container'>
+    //             <p>Length</p>
+    //             <p>:</p>
+    //             <input
+    //                 type="text"
+    //                 value={length}
+    //                 onChange={(e) => setLength(e.target.value)}
+    //             />
+    //         <button onClick={handleClick}>Generate Password</button>
+    //         </div>
+    //         <div className='pass-container'>
+    //             <p>PASSWORD </p>
+    //             <p>:</p>
+    //             <input
+    //                 type="text"
+    //                 value={message}
+    //                 readOnly
+    //             />
+    //             {/* {message.map((each, index) => (
+    //                 <p key={index}>name {index}: {each.name}</p>
+    //             ))} */}
+    //             {/* <p>{message}</p> */}
+    //         </div>
+    //     </div>
+    // );
+
 }
 
 export default App;
